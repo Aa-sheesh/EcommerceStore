8bbbae896f198233021fcf8dd5501305250cc623
Aa-sheesh <aashish4912345@gmail.com>
2024-11-24 19:47:17 +0530

    Backend Development Complete with testing

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,7 +1,6 @@
-import Redis from "ioredis"
-import dotenv from "dotenv"
+import Redis from "ioredis";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const redis = new Redis(process.env.UPSTASH_REDIS_URI);
-await redis.set('foo', 'bar');
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.route.js';
+import productRoutes from './routes/product.route.js';
+import cartRoutes from './routes/cart.route.js';
+import couponRoutes from './routes/coupon.route.js';
+import paymentRoutes from './routes/payment.route.js';
+import analyticsRoutes from './routes/analytics.route.js';
+
 import {connectDB} from './lib/db.js';
+
 dotenv.config();
 
 
@@ -9,7 +18,14 @@ const app=express();
 const PORT=process.env.PORT||5000;
 
 app.use(express.json());//allows you to parse body of the request
+app.use(cookieParser());//allows you to parse cookies
+
 app.use("/api/auth",authRoutes);
+app.use("/api/products",productRoutes);
+app.use("/api/cart",cartRoutes);
+app.use("/api/coupons",couponRoutes);
+app.use("/api/payments",paymentRoutes);
+app.use("/api/analytics",analyticsRoutes);
 
 app.listen(5000,()=>{
     console.log(`http://localhost:${PORT}`);
